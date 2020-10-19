da5d8fc8a02c7ddd812b59fc33a26223ae37f5a9
derektrom <54723488+derektrom@users.noreply.github.com>
2020-10-18 22:35:58 -0500

    adds in files, adds comments to hw7-derektrom.c, test.c

--- hw7-derektrom/hw7-derektrom.c
+++ hw7-derektrom/hw7-derektrom.c
@@ -36,50 +36,54 @@ float sharedMem = 0.0f;
 int main(int argc, char** argv);
 void* threadFunction(void* arg);
 
-
+//thread function
 void* threadFunction(void* arg){
     //Variable declarations for thread function
     const char* threadName = (char*) arg;
-    char fileName[12];
-    char value[10];
+    char fileName[10];
     char withdrawDeposit;
     float badSpending;
 
     sprintf(fileName, "%s.in", threadName);
     FILE *in = fopen(fileName, "r");
-    printf("In thread %s",threadName);
-
     while(fscanf(in, "%c", &withdrawDeposit) != EOF)
     {
+        //switch statement for enter critical section, deposit
+        //withdraw or exit critical section
         switch (withdrawDeposit)
         {
         case 'R':
             fscanf(in, "\n");
+            //lock the mutex
             pthread_mutex_lock(&cond);
             break;
         case '+':
             fscanf(in, "%f\n", &badSpending);
+            //deposit money
             sharedMem += badSpending;
             break;
         case '-':
             fscanf(in, "%f\n", &badSpending);
+            //withdraw money
             sharedMem -= badSpending;
             break;
         case 'W':
            printf("Account balance after thread %s is $%.2f\n", threadName, sharedMem);
-           pthread_mutex_unlock(&cond);
            fscanf(in, "\n");
-           usleep(1);
-           //sleep(.25);
+           //unlock mutex
+           pthread_mutex_unlock(&cond);
+           //sleep 1/4 second to create randomness
+           sleep(.25);
            break;           
         default:
+            //default if the wrong symbol is in file
             printf("Unreadable command in file %s: %c\n", threadName, withdrawDeposit);
             break;
         }            
     }
-     
+    //close file when finished 
     fclose(in);
-
+    //exit thread
     pthread_exit(NULL);
     
     
@@ -88,13 +92,16 @@ void* threadFunction(void* arg){
 //Main Thread Function, AKA Morticia
 int main(int argc, char** argv){
     printf("Hello from Morticia, say hello to my monsters!!\n");
+    //initialize mutex
     pthread_mutex_init(&cond, NULL);
+    //array of threads
     pthread_t threads[NUM_THREADS];
+    //create threads
     for (int i = 0;i<NUM_THREADS;i++){
         pthread_create(&threads[i], NULL, threadFunction, (void*) monsters[i]);
-        printf("Created %s thread bwahaha\n", monsters[i]);
-        
+        printf("Created %s thread bwahaha\n", monsters[i]);    
     }
+    //join threads
     for (int i = 0; i<NUM_THREADS;i++){
         pthread_join(threads[i], NULL);
         printf("%s's thread finished spending terribly...bwahaha\n", monsters[i]);
